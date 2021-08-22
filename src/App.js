df46934d3fe7b52920ac36769618f06ceca67c9c
Danni Wilton <danniwilton@gmail.com>
2021-08-22 11:25:05 +0800

    styling navbar and title, added favicon

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import Navbar from './Components/Navigation';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Home from './components/pages/Home';
+import Home from './Components/Home.js';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
+    // <ApolloProvider client={client}>
     <Router>
     <Navbar />
     <Switch>
       <Route path= '/' exact component={Home} />
-      <Route path='/Projects' component={Projects} />
+      {/* <Route path='/Projects' component={Projects} />
       <Route path='/Resume' component={Resume} />
-      <Route path='/Contact' component={Contact} />
+      <Route path='/Contact' component={Contact} /> */}
     </Switch>
     </Router>    
-    </ApolloProvider>
+    // </ApolloProvider>
   );
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../App.css';
-import { Button } from './button';
 import './Home.css';
 
 
@@ -9,10 +8,6 @@ function LandingPage() {
         <div className='hero-container'>
             <h1 className="landing-heading">Danni Wilton</h1>
             <p>Full Stack Web Developer</p>
-            
-            <div className="hero-btns">
-                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Contact</Button>
-            </div> 
             
         </div>
         
