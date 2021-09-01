ab8fd152be419c4967aef1c080e7abac8cfb9393
Danni Wilton <danniwilton@gmail.com>
2021-09-02 00:16:15 +0800

    added links to projects

--- src/Components/pages/Projects.js
+++ src/Components/pages/Projects.js
@@ -12,21 +12,39 @@ function Projects() {
         <div className="project-container">
             <div className="project">
                 <img className="Projects" src={skullstylist1} alt="skull stylist project"/>
+                <a href="https://github.com/DanniWilton/SkullStylist">Skull Stylist Github</a>
+                
+                <a href="https://github.com/DanniWilton/SkullStylist">Heroku Deployment</a>
+                
             </div>
             <div className="project">
                 <img className="Projects" src={jsquiz} alt="javascript quiz"/>
+                <a href="https://github.com/DanniWilton/JavascriptQuiz">Javascript Quiz Github</a>
+                <a href="https://danniwilton.github.io/JavascriptQuiz">Website</a>
             </div>
+
+
             <div className="project">
                 <img className="Projects" src={passwordgen} alt="password generator"/>
+                <a  href="https://github.com/DanniWilton/PasswordGenerator">Password Generator Github</a>
+                <a href="https://danniwilton.github.io/PasswordGenerator/">Website</a>
             </div>
+            
             <div className="project">
                 <img className="Projects" src={evento} alt="evento- the event searcher"/>
+                <a href="https://github.com/Au-RiZeR/Event-Searcher">Evento Github</a>
+                <a href="https://au-rizer.github.io/Event-Searcher">Website</a>
             </div>
             <div className="project">
                 <img className="Projects" src={readmegen} alt="read me generator"/>
+                <a href="https://github.com/DanniWilton/readmegenerator">ReadMe Github</a>
+                <a href="https://drive.google.com/file/d/1SfO6QB4M4GnT7qEuMdOUICKt2k_vA2pK/view">Video of application</a>
             </div>
             <div className="project">
                 <img className="Projects" src={velvetsoapbox} alt="velvet soap box- a place to add events"/>
+                <a  href="https://github.com/Serotonin-Seekers/Evento2">The Velvet Soap Box Github</a>
+                <a href="https://event-woo-serotonin-seekers.herokuapp.com">Heroku Deployment</a>
+
             </div>
         </div>
     )
